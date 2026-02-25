f4884f8bab33c639197e2b525942cd6111891e1d
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-26 00:07:33 +0530

    1.2 - Connect to mongodb using mongoose

---
+++ configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected ...");
+    }); // Event get triggered once we connected to 'Mongodb Server'.
+    await mongoose.connect(`${process.env.MONGODB_URI}/car-rental`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config"; // 'dotenv' library config allows to access 'ENV' variables.
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
 // Initialize Express app
 const app = express();
+
+// Connect to mongodb database
+connectDB();
 
 // Middlewares
 app.use(cors());
