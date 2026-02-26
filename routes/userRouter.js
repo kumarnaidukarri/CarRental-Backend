197d3374d5bb12768859f0ebff8fa55fbb0149a9
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-26 11:37:35 +0530

    3.1 - Added 'User Router' and 'registerUser' 'login' controllerfunctions in it

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,6 +4,7 @@ import express from "express";
 import { registerUser, loginUser } from "../controllers/userController.js";
 
 const userRouter = express.Router();
+
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 
