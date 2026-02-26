68b5f052bb18fe49e65c38df32d5eadc1ceb1529
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-26 11:34:29 +0530

    3 - Explored 'Routers Routes' folder concept

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config"; // 'dotenv' library config allows to access 'ENV' variables.
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRouter.js";
 
 // Initialize Express app
 const app = express();
@@ -11,12 +12,16 @@ connectDB();
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); // all requests will pass into this middleware prior to server.
+app.use(
+  express.json(),
+); /* Middleware tells Express to automatically 'Parse Incoming JSON Request Bodies' and convert into 'Javascript Object'. then, stores it in req.body. 
+i.e, request body -> middleware convert 'JSON' into 'Javascript Object'. */
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello There");
 });
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
